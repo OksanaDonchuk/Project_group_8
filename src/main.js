071aad1aee71428c2ecddd3b58aa4c0e6f3881f4
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2023-09-02 21:40:26 +0300

    Update structure

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import './js/reviews-swiper';
 import './js/mobile-menu';
-import './modal';
-
+import './js/modal';
 import './js/modal-privacy';
 import './js/modal-license';
 import './js/teachers-swiper';
