4db2905ed92475773fc0b030e13c930fd94c05d1
sem-aa <66129330+sem-aa@users.noreply.github.com>
2023-09-02 14:08:01 +0300

    fix modal team

--- src/modal.js
+++ src/modal.js
@@ -1,16 +1,15 @@
+const openModalBtn = document.querySelector('.data-modal-open');
+const closeModalBtn = document.querySelector('[data-modal-close]');
+const modal = document.querySelector('[data-modal]');
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector(".data-modal-open"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+openModalBtn.addEventListener('click', openModal);
+closeModalBtn.addEventListener('click', closeModal);
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    console.log("click btn")
-  }
-})();
+function openModal() {
+  modal.classList.remove('is-hidden');
+  document.body.style.overflow = 'hidden';
+}
+function closeModal() {
+  modal.classList.add('is-hidden');
+  document.body.style.overflow = 'visible';
+}
